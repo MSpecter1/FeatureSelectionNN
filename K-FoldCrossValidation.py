dde6a921090dcfaada03d239a7ab8a61cad1d88d
MSpecter1git config --global user.name Mspecter1git config --global user.name MSpecter1 <michaelhe61@gmail.com>
2023-05-31 15:28:25 -0700

    Added test cases  and validated for small/large datasets

--- K-FoldCrossValidation.py
+++ K-FoldCrossValidation.py
@@ -4,35 +4,18 @@ import math
 import time
 
 
-# euclidean distance using linear algebra : https://stackoverflow.com/questions/60574862/calculating-pairwise-euclidean-distance-between-all-the-rows-of-a-dataframe
+# Euclidean distance using linear algebra : https://stackoverflow.com/questions/60574862/calculating-pairwise-euclidean-distance-between-all-the-rows-of-a-dataframe
+# Basic structure from Professor Keogh's project 2 briefing : https://www.dropbox.com/sh/ftzvcnntl2j5eiu/AADbGDjXQFeXnqmfAsOsppTIa/Project_2_Briefing.pptx?dl=0
 def findAccuracy(input_df, feature_subset):
     row_size = input_df.shape[0]
     col_size = input_df.shape[1]
     correct_cnt = 0
 
     # Remove features not in subset
-    # df = input_df.copy()
-    # for col in range(1, col_size):
-    #     if (col not in feature_subset):
-    #         df[col].values[:] = 0
     np_df = np.copy(input_df)
     for col in range(1, col_size):
         if (col not in feature_subset):
             np_df[:,col]= 0
-
-    # MANUAL TEST
-    # test1 = df.iloc[0, 1:col_size]
-    # test2 = df.iloc[1, 1:col_size]
-    # dist = np.linalg.norm(test1-test2)
-    # print(dist)
-    # dist2 = math.sqrt(pow((df.iat[0,1]-df.iat[1,1]),2)+pow((df.iat[0,2]-df.iat[1,2]),2)+pow((df.iat[0,3]-df.iat[1,3]),2))
-    # print(dist2)
-
-    # np_df = df.to_numpy()
-    # cur_instance = np_df[0, :]
-    # cur_instance2 = np_df[1, :]
-    # dist = np.linalg.norm(cur_instance - cur_instance2)
-    # print(dist)
 
     for i in range(0,row_size):
         # cur_class = df.iat[i,0]
@@ -41,7 +24,6 @@ def findAccuracy(input_df, feature_subset):
         nn_index = None
         nn_class = None
 
-        # cur_instance = df.iloc[i, 1:col_size]
         cur_instance = np_df[i, 1:]
         
         # test one data instance, i, against all others
@@ -60,13 +42,79 @@ def findAccuracy(input_df, feature_subset):
             correct_cnt+=1
     return correct_cnt/row_size
 
-df = pd.read_csv(r"CS170_small_Data__32.txt",header=None, delim_whitespace=True)
-df = df.to_numpy()
-feature_subset = {3,1,5} # EXPECTED VAL: 0.954
+class Test:
+    def testS32(self):
+        df = pd.read_csv(r"CS170_small_Data__32.txt",header=None, delim_whitespace=True)
+        df = df.to_numpy()
+        feature_subset = {3,1,5} # EXPECTED VAL: 0.954
 
-start = time.time()
-result = findAccuracy(df, feature_subset)
-print("Execution Time: ", time.time()-start)
-print(result)
+        start = time.time()
+        result = findAccuracy(df, feature_subset)
+        print("Execution Time: ", time.time()-start)
+        if(result==0.954): return True
+        return False
+    
+    def testS33(self):
+        df = pd.read_csv(r"CS170_small_Data__33.txt",header=None, delim_whitespace=True)
+        df = df.to_numpy()
+        feature_subset = {8,7,3} # EXPECTED VAL: 0.949
 
+        start = time.time()
+        result = findAccuracy(df, feature_subset)
+        print("Execution Time: ", time.time()-start)
+        if(result==0.949): return True
+        return False
+    
+    def testL32(self):
+        df = pd.read_csv(r"CS170_large_Data__32.txt",header=None, delim_whitespace=True)
+        df = df.to_numpy()
+        feature_subset = {3,7,6} # EXPECTED VAL: 0.963
 
+        start = time.time()
+        result = findAccuracy(df, feature_subset)
+        print("Execution Time: ", time.time()-start)
+        if(result==0.963): return True
+        return False
+    
+    def testL33(self):
+        df = pd.read_csv(r"CS170_large_Data__33.txt",header=None, delim_whitespace=True)
+        df = df.to_numpy()
+        feature_subset = {4,5,10} # EXPECTED VAL: 0.9655
+
+        start = time.time()
+        result = findAccuracy(df, feature_subset)
+        print("Execution Time: ", time.time()-start)
+        if(result==0.9655): return True
+        return False
+    
+    def testXL1(self):
+        df = pd.read_csv(r"CS170_XXXlarge_Data__1.txt",header=None, delim_whitespace=True)
+        df = df.to_numpy()
+        feature_subset = {4,5,10} # EXPECTED VAL: 0.9655
+
+        start = time.time()
+        result = findAccuracy(df, feature_subset)
+        print("Execution Time: ", time.time()-start)
+        print("Result: ", result)
+        # if(result==0.9655): return True
+        # return False
+    
+    def test(self):
+        fail = 0
+        if not self.testS32(): 
+            print("S32 failed")
+            fail+=1
+        if not self.testS33(): 
+            print("S33 failed")
+            fail+=1
+        if not self.testL32(): 
+            print("L32 failed")
+            fail+=1
+        if not self.testL33(): 
+            print("L33 failed")
+            fail+=1
+        print("Tests failed: ", fail)
+
+# test = Test()
+# test.test()
+# test.testXL1()
